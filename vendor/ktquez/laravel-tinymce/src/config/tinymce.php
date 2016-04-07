4fb14143d1f7da109f762b44effe9736e4465d68
joel <joel.fernandes@theretentionpeople.com>
2016-04-07 20:53:10 +0100

    pushing vendor folder

---
+++ vendor/ktquez/laravel-tinymce/src/config/tinymce.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+	'cdn' => config('app.url').'/vendor/js/tinymce/tinymce.min.js',
+
+	'params' => [
+		"selector" => "#tinymce",
+		"theme" => "modern",
+		"skin" => "lightgray",
+		"plugins" => [
+	         "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+	         "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+	         "save table contextmenu directionality emoticons template paste textcolor"
+		],
+		"toolbar" => "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor emoticons",
+	]
+
+];
